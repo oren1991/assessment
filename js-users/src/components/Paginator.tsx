f81aadd340227ae92f9592297f169cea45a1a018
Oren Arbell <oren.arbell@hu.ibm.com>
2021-03-07 09:26:00 +0100

    clean paginator

--- js-users/src/components/Paginator.tsx
+++ js-users/src/components/Paginator.tsx
@@ -50,7 +50,6 @@ export const Paginator = <T,>({
         <PaginatorContainer isLoading={isLoading}>
             {children(currentItems)}
             <PaginatorNavigator>
-                {" "}
                 <PaginatorButton
                     data-testid="prev-page"
                     onClick={handlePrevPage}
